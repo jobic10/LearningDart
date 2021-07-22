2ad2422609d4c267f389086cc433feea66fd629f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-22 13:14:07 +0100

    Variable and Constants

---
+++ bin/Day 1/var_const.dart
@@ -0,0 +1,15 @@
+void main() {
+  var number = 8;
+  var result = 1 + 2;
+  num j = 3;
+  j = 43.2;
+  var name = 'Job';
+  name = 'js';
+  const key = 127;
+  final keys = 233;
+  print(key);
+  final d = DateTime.now();
+  print(d);
+  print(d.toUtc());
+  print(d.toUtc().toString());
+}
