d3821cc66e0f9b1141be946fec4fcf9b3c7d7122
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-22 15:57:22 +0100

    Override

--- bin/Day 1/oop_intro.dart
+++ bin/Day 1/oop_intro.dart
@@ -3,8 +3,22 @@ class Student {
   String dept;
   int age;
   bool hasPaid;
+
+  @override
+  String toString() {
+    return 'Student $name';
+  }
+
+  String toJson() {
+    return '{name:$name,dept:$dept,age:$age,hasPaid:$hasPaid}';
+  }
 }
 
 void main() {
   var job = Student();
+  print(job);
+  print(job.toJson());
+
+  // Object Serialization
+  // adding JSON
 }
