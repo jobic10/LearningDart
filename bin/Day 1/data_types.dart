2f48f1e3c5e3f8659cd033d290f046fb08f3546f
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-22 13:28:06 +0100

    As

--- bin/Day 1/data_types.dart
+++ bin/Day 1/data_types.dart
@@ -4,5 +4,17 @@ void main() {
   var ageString = age.toString();
   ageString = '2f';
   var ageBack = int.tryParse(ageString) ?? 1;
-  print(ageBack);
+  // print(ageBack);
+
+  var x = 23;
+  var y = 43.23;
+  var result = x * y;
+  print(result);
+  print(result.runtimeType);
+  // Operations on both int and double returns double
+
+  num value = 7;
+  var valueInteger = value as int;
+  print(valueInteger);
+  print(valueInteger.isEven);
 }
