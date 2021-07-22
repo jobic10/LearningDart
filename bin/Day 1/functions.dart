676a73dfb2d14fdba5e3de5b35d2cec746220bb8
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-22 14:56:36 +0100

    More on functions

--- bin/Day 1/functions.dart
+++ bin/Day 1/functions.dart
@@ -2,12 +2,20 @@ void main() {
   echo(str: 'Job');
   var res = plus(2, 3);
   echo(str: res.toString());
+  print(loadProfile('Job'));
+  print(loadProfile('Liz', 'Manager'));
 }
 
 void echo({String str}) {
+  // Has side effect
   print(str);
 }
 
 int plus(int a, int c) {
+  // No side effect
   return a + c;
+}
+
+String loadProfile(String name, [String position = 'Member']) {
+  return '$name - $position';
 }
