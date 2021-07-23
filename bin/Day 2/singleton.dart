27fe90f17dbd019033ae0abeffc00c584e939647
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-23 12:57:13 +0100

    Singleton

---
+++ bin/Day 2/singleton.dart
@@ -0,0 +1,12 @@
+class Database {
+  Database._();
+  static final Database _instance = Database._();
+  factory Database() {
+    return _instance;
+  }
+}
+
+void main() {
+  var db = Database();
+  print(db);
+}
