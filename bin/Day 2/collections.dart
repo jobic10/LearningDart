02515b069a5eb188837cd42d6aeba65edc163886
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-23 14:22:01 +0100

    Expand

--- bin/Day 2/collections.dart
+++ bin/Day 2/collections.dart
@@ -1,11 +1,21 @@
 void main() {
   // List
-  List pl = <String>['Dart', 'Java', 'Python'];
+  var pl = ['Dart', 'Java', 'Python'];
   pl.add('newValue');
-  pl.addAll(<String>['iterable', 'iter2']);
+  pl.addAll(['iterable', 'iter2']);
   print(pl);
   print('***' * 16);
-  print(pl.where((element) => element.length > 4));
-  print('***' * 16);
-  print(pl);
+  // print(pl.where((element) => element.length > 4));
+  print(pl.contains('Dart'));
+  print(pl.elementAt(2));
+  print('-----' * 16);
+  print(pl.every((element) => element.length > 4));
+  print('-----' * 16);
+  // pl.expand((element) => element);
+  var pairs = [
+    [1, 2],
+    [3, 4]
+  ];
+  var flattened = pairs.expand((pair) => pair).toList();
+  print(flattened);
 }
