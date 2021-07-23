5121d81006162c80411cfd123ba2a2d7235f1721
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-23 10:23:59 +0100

    Forwarding Constructors

--- bin/Day 2/oop_named_cons.dart
+++ bin/Day 2/oop_named_cons.dart
@@ -4,13 +4,16 @@ class Student {
   int age;
   int level;
 
+  Student({this.name, this.age, this.dept, this.level});
+
   Student.utme({this.name, this.dept}) {
     level = 100;
   }
-  Student.direct_entry({this.name, this.dept}) {
-    level = 200;
-  }
-
+  // Student.direct_entry({this.name, this.dept}) {
+  //   level = 200;
+  // }
+  Student.direct_entry() : this(level: 200);
+  Student.remedial() : this(level: 100);
   @override
   String toString() {
     return '$name is in $level -> $dept';
@@ -18,7 +21,9 @@ class Student {
 }
 
 void main() {
-  var Job = Student.direct_entry(
-      name: 'Owonubi Job Sunday', dept: 'Computer Science');
+  var Job = Student.direct_entry()
+    ..name = 'Owonubi Job'
+    ..age = 123
+    ..dept = 'Computer Science';
   print(Job);
 }
