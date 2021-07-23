83130991719b4897dd3118c33b89528e5fdb7f20
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-23 17:06:45 +0100

    ⚗️ Exp

--- bin/Day 2/async.dart
+++ bin/Day 2/async.dart
@@ -1,10 +1,10 @@
 void main() async {
   print('Before Future');
-  final future =
-      await Future<double>.delayed(Duration(seconds: 2), () => throw ('s'))
-          .then((value) => print(value))
-          .whenComplete(() => print('Future is complete'))
-          .catchError((error) => print('Error $error'));
-
+  final future = await Future<double>.delayed(
+          Duration(seconds: 2), () => throw ('Exception'))
+      .then((value) => value)
+      .whenComplete(() => print('Future is complete'))
+      .catchError((error) => 0.0);
+  print('Value is $future');
   print('After Future');
 }
