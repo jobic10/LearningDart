3e59a5bec3c8bc2bc72a7cc3e5330b3ae992a359
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-23 12:53:18 +0100

    Getters and setters done

--- bin/Day 2/client.dart
+++ bin/Day 2/client.dart
@@ -1,15 +1,21 @@
 class Client {
-  final int _id;
-  final String _name;
-  final double _accountBalance;
+  int _id;
+  String _name;
+  double _accountBalance;
 
   Client({int id, String name, double accountBalance})
       : _id = id,
         _name = name,
         _accountBalance = accountBalance;
+  // Getters
   int get id => _id;
   String get name => _name;
   double get accountBalance => _accountBalance;
+
+  // Setters
+  set clientName(String name) => _name = name;
+  set clientId(int id) => _id = id;
+  set clientBalance(double bal) => _accountBalance = bal;
   @override
   String toString() {
     return '$_name with ID $_id has $_accountBalance balance';
--- bin/Day 2/getters_setters.dart
+++ bin/Day 2/getters_setters.dart
@@ -3,4 +3,6 @@ import 'client.dart';
 void main() {
   var job = Client(accountBalance: 7287.892, id: 127, name: 'Job');
   print(job.name);
+  job.clientName = 'Owonubi Job SUnday';
+  print(job.name);
 }
