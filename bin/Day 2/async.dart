dc74c3cbe3f604736d1d3621806d5e8abb1bff4e
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-23 17:02:59 +0100

    Async and Await

--- bin/Day 2/async.dart
+++ bin/Day 2/async.dart
@@ -1,9 +1,10 @@
-void main() {
+void main() async {
   print('Before Future');
-  final future = Future<double>.delayed(Duration(seconds: 2), () => throw ('s'))
-      .then((value) => print(value))
-      .whenComplete(() => print('Future is complete'))
-      .catchError((error) => print('Error $error'));
+  final future =
+      await Future<double>.delayed(Duration(seconds: 2), () => throw ('s'))
+          .then((value) => print(value))
+          .whenComplete(() => print('Future is complete'))
+          .catchError((error) => print('Error $error'));
 
-  print('Before Future');
+  print('After Future');
 }
