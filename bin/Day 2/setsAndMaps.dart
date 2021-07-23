9f4462df54a7189a5de57e73fb4071690f82e2ce
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-23 15:22:20 +0100

    Reduce

--- bin/Day 2/setsAndMaps.dart
+++ bin/Day 2/setsAndMaps.dart
@@ -1,6 +1,12 @@
 void main() {
   var mySet = {31, 234, 45};
+  print(mySet.map((e) => e * 3));
   print(mySet);
   var myMap = <String, int>{'Job': 127};
   print(myMap);
+
+  // Reduce
+  var amounts = [121, 324, 343, 46, 745, 343, 56, 36, 23];
+  final total = amounts.reduce((value, element) => value + element);
+  print(total);
 }
