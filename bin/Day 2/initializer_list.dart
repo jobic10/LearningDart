861ef1075370f1406e8eeedb78540780b3b74733
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-23 10:31:30 +0100

    Initializer List

---
+++ bin/Day 2/initializer_list.dart
@@ -0,0 +1,20 @@
+void main() {
+  var client = Client(id: 127, name: 'Owonubi', accountBalance: 39738);
+  print(client);
+}
+
+class Client {
+  int _id;
+  String _name;
+  double _accountBalance;
+
+  Client({int id, String name, double accountBalance})
+      : _id = id,
+        _name = name,
+        _accountBalance = accountBalance;
+
+  @override
+  String toString() {
+    return '$_name with ID $_id has $_accountBalance balance';
+  }
+}
