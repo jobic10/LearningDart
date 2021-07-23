3b73b3746f387be5c07d6cd16b5676de6ef346b1
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-23 15:39:57 +0100

    Overriding

--- bin/Day 2/muli-level-inheritance.dart
+++ bin/Day 2/muli-level-inheritance.dart
@@ -2,14 +2,29 @@ class Car {
   String name;
   double highestSpeed;
   Car(this.name, this.highestSpeed);
+
+  void move() {
+    print('I am moving');
+  }
 }
 
 class SUV extends Car {
   String owner;
   SUV(this.owner, name, highestSpeed) : super(name, highestSpeed);
+
+  void jump() {
+    print('SUV is jumping');
+  }
+
+  @override
+  void move() {
+    super.move();
+    print('I am still moving');
+  }
 }
 
 void main() {
   var myCar = SUV('Job', 'SUV 2018 AE', 289.2);
   print(myCar.highestSpeed);
+  myCar.move();
 }
