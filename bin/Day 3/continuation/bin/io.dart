589dd72c2bed253e761fb62c513deb5795904f4b
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-24 20:26:31 +0100

    🔥 Cool IO

--- bin/Day 3/continuation/bin/io.dart
+++ bin/Day 3/continuation/bin/io.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
 void main() async {
-  var file = Directory('example.txt');
+  var file = Directory('/');
   if (await file.exists()) {
+    for (var f in file.listSync()) {
+      print(f.path);
+    }
     print('Yeah');
   } else {
     print('Nah');
