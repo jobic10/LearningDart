04da7adf98fdfb1e0853b645e977a65e73afaf19
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-24 08:55:58 +0100

    Late keyword

---
+++ bin/Day 3/continuation/bin/car.dart
@@ -0,0 +1,18 @@
+class Car {
+  late int _speed;
+  void accelerate() {
+    _speed = 50;
+    print('Accelerating Speed : $_speed');
+  }
+
+  void steer() {
+    _speed += 10;
+    print('Steering Speed : $_speed');
+  }
+}
+
+void main() {
+  var camry = Car();
+  camry.accelerate();
+  camry.steer();
+}
