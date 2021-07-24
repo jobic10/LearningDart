d28308962fdd00f78191416945efb87df9d03924
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-24 08:29:30 +0100

    Null safety test

--- bin/Day 3/continuation/bin/continuation.dart
+++ bin/Day 3/continuation/bin/continuation.dart
@@ -1,3 +1,9 @@
+void driveTwoSeat(String driver, [String? passenger]) {
+  print('$driver driving with $passenger');
+}
+
 void main(List<String> arguments) {
   print('Hello world!');
+  driveTwoSeat('Job');
+  driveTwoSeat('Job', 'Liz');
 }
