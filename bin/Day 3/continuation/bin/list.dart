46fe7e36367fb182321832190b4030101ecf83d6
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-07-24 12:17:27 +0100

    ✨ New

--- bin/Day 3/continuation/bin/list.dart
+++ bin/Day 3/continuation/bin/list.dart
@@ -2,10 +2,18 @@ import 'dart:math';
 
 // Continuation From Day 1 List
 void main() {
-  var myList = List.filled(20, 30);
+  var myList = List.filled(20, 30, growable: true);
   var random = Random();
   // myList.add(10);
   var myList2 = List.generate(20, (index) => random.nextInt(30));
-  print('List : $myList');
+  print('List 1 : $myList');
   print('List 2 : $myList2');
+  var myList3 = [...myList2];
+  myList3.add(20);
+  var myList4 = myList;
+  myList4.add(127);
+  print('List 1 : $myList');
+  print('List 2 : $myList2');
+  print('List 3 : $myList3');
+  print('List 4 : $myList4');
 }
